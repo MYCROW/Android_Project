c4cca89edd6ffe9170dc1ebe686c2a87522e9082
MYCROW <853822587@qq.com>
2018-05-14 14:26:30 +0800

    A demo about activity life cycle。

--- app/src/main/java/com/example/crow/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/crow/myapplication/SecondActivity.java
@@ -1,7 +1,6 @@
 package com.example.crow.myapplication;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,26 +9,26 @@ import android.widget.Button;
 
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends Activity {
-    private Button bt1,bt2;
-    static  int index = 0;
+public class SecondActivity extends Activity {
+    private Button bt1,bt2,bt3;
+    static int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         index++;
-        Log.i(TAG,MainActivity.class+"Called onCreate "+index);
-        bt1 = (Button)findViewById(R.id.button1);
-        bt2 = (Button)findViewById(R.id.button2);
-
+        Log.i(TAG,SecondActivity.class+"Called onCreate "+index);
+        bt1 = (Button)findViewById(R.id.button3);
+        bt2 = (Button)findViewById(R.id.button4);
+        bt3 = (Button)findViewById(R.id.button5);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            public void onClick(View v) {//返回上一活动？
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-        bt2.setOnClickListener(new View.OnClickListener() {
+        bt2.setOnClickListener(new View.OnClickListener() {//返回主界面
             @Override
             public void onClick(View v) {
                 Intent it = new Intent();
@@ -38,38 +37,42 @@ public class MainActivity extends Activity {
                 startActivity(it);
             }
         });
-
-
+        bt3.setOnClickListener(new View.OnClickListener() {//结束当前活动
+            @Override
+            public void onClick(View v) {
+                SecondActivity.this.finish();
+            }
+        });
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,MainActivity.class+"Called onStart "+index);
+        Log.i(TAG,SecondActivity.class+"Called onStart "+index);
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,MainActivity.class+"Called onResume "+index);
+        Log.i(TAG,SecondActivity.class+"Called onResume "+index);
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,MainActivity.class+"Called onPause "+index);
+        Log.i(TAG,SecondActivity.class+"Called onPause "+index);
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,MainActivity.class+"Called onStop "+index);
+        Log.i(TAG,SecondActivity.class+"Called onStop "+index);
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,MainActivity.class+"Called onDestroy "+index);
+        Log.i(TAG,SecondActivity.class+"Called onDestroy "+index);
     }
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,MainActivity.class+"Called onRestart "+index);
+        Log.i(TAG,SecondActivity.class+"Called onRestart "+index);
     }
-
 }
